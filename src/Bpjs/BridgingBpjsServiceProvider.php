04d374c6771c4efde61bf909cea029f97991294b
virusphp <packercyber@gmail.com>
2023-01-05 08:29:04 +0700

    fixed vendor publish for laravel

--- src/Bpjs/BridgingBpjsServiceProvider.php
+++ src/Bpjs/BridgingBpjsServiceProvider.php
@@ -26,8 +26,9 @@ class BridgingBpjsServiceProvider extends ServiceProvider
        include __DIR__.'../../routes.php';
        
        $this->publishes([
-            __DIR__.'../../../../../config/vclaim.php' => config_path('vclaim.php'),
-            __DIR__.'../../../../../config/antrol.php' => config_path('antrol.php'),
+            __DIR__.'../../../config/vclaim.php' => config_path('vclaim.php'),
+            __DIR__.'../../../config/antrol.php' => config_path('antrol.php'),
+            __DIR__.'../../../config/kemkes.php' => config_path('kemkes.php'),
         ], 'config');
     }
 }
